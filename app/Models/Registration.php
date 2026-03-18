e71e41b08c18f2363b07ed70f3cca4039481d802
Gedion Daniel <gediondaniel321@gmail.com>
2026-03-18 04:59:11 +0100

    feat: add Registration model and migration (status, waiting_position)

---
+++ app/Models/Registration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Registration extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'workshop_id',
+        'user_id',
+        'status',
+        'waiting_position',
+    ];
+
+    public function workshop()
+    {
+        return $this->belongsTo(Workshop::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
